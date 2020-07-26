464ef48107a0d8b95f48a1e03213cb9014af37ba
Marvin Luchs <marvin@luchs.org>
2020-07-26 13:28:09 +0200

    fix: do not use relative links in navbar

--- apps/demo/src/app/navbar/navbar.tsx
+++ apps/demo/src/app/navbar/navbar.tsx
@@ -2,30 +2,30 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 /* eslint-disable-next-line */
-export interface NavbarProps {}
+export interface NavbarProps { }
 
 export const Navbar = (props: NavbarProps) => {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary mb-5">
-        <div className="container">
-            <NavLink className="navbar-brand" to="/">Demo</NavLink>
-            <ul className="navbar-nav">
-                <li className="nav-item">
-                    <NavLink className="nav-link" to="products">Catalog</NavLink>
-                </li>
-                <li className="nav-item">
-                    <NavLink className="nav-link" to="shopping-cart">Shopping Cart</NavLink>
-                </li>
-                <li className="nav-item">
-                    <NavLink className="nav-link" to="orders">Orders</NavLink>
-                </li>
-                <li className="nav-item">
-                    <NavLink className="nav-link" to="user-profile">User Profile</NavLink>
-                </li>
-            </ul>
-        </div>
-    </nav>
-  );
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary mb-5">
+            <div className="container">
+                <NavLink className="navbar-brand" to="/">Demo</NavLink>
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="/products">Catalog</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="/shopping-cart">Shopping Cart</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="/orders">Orders</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="/user-profile">User Profile</NavLink>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
