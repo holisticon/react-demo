46dce06c5578b8f114e26411277ae650c9ddd87d
Paul K Deuster <paul.deuster@holisticon.de>
2021-08-26 13:54:42 +0200

    test: react testing lib setup

---
+++ libs/testing/src/lib/testing.tsx
@@ -0,0 +1,15 @@
+import { render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import React, { PropsWithChildren } from 'react';
+import { Router } from 'react-router-dom';
+
+
+const Wrapper = ({children}: PropsWithChildren<{}>) => {
+  const history = createMemoryHistory();
+  return (<Router history={history}>
+    {children}
+  </Router>)
+}
+
+const customRender: typeof render = (ui, options) => 
+  render(ui, {wrapper: Wrapper, ...options})
