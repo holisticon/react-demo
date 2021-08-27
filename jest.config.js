7c036ce081a4253bb3f428f149b1145431f2e0ea
Marvin Luchs <marvin.luchs@holisticon.de>
2021-08-27 12:39:52 +0200

    chore: update to Nx 11

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
-  transform: {
-    '^.+\\.(ts|js|html)$': 'ts-jest',
-  },
-  resolver: '@nrwl/jest/plugins/resolver',
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageReporters: ['html'],
-  moduleNameMapper: {
-      'lodash-es': 'lodash'
-  }
+  projects: [
+    '<rootDir>/apps/demo',
+    '<rootDir>/libs/homepage',
+    '<rootDir>/libs/products',
+    '<rootDir>/libs/shopping-cart',
+    '<rootDir>/libs/orders',
+    '<rootDir>/libs/user-profile',
+    '<rootDir>/libs/resource',
+    '<rootDir>/libs/testing',
+  ],
 };
