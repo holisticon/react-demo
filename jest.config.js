aa451104362b5eca99dddc63a0a5c2c796a98bfc
Marvin Luchs <marvin.luchs@holisticon.de>
2021-08-27 12:45:33 +0200

    chore: update to Nx 12

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,15 @@
+const { getJestProjects } = require('@nrwl/jest');
+
 module.exports = {
-  projects: [
-    '<rootDir>/apps/demo',
-    '<rootDir>/libs/homepage',
-    '<rootDir>/libs/products',
-    '<rootDir>/libs/shopping-cart',
-    '<rootDir>/libs/orders',
-    '<rootDir>/libs/user-profile',
-    '<rootDir>/libs/resource',
-    '<rootDir>/libs/testing',
-  ],
+    projects: [
+        ...getJestProjects(),
+        '<rootDir>/apps/demo',
+        '<rootDir>/libs/homepage',
+        '<rootDir>/libs/products',
+        '<rootDir>/libs/shopping-cart',
+        '<rootDir>/libs/orders',
+        '<rootDir>/libs/user-profile',
+        '<rootDir>/libs/resource',
+        '<rootDir>/libs/testing',
+    ],
 };
